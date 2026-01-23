3538d1bc86685a76952e1e21f78d3d7523d181b8
yerrasivakumar143 <yerrasiva123456@gmail.com>
2026-01-23 12:24:54 +0530

    add token into single user

--- e-commerce/routes/authRoutes.js
+++ e-commerce/routes/authRoutes.js
@@ -2,12 +2,12 @@ import express from 'express';
 import { signup, loginUser,singleuser } from '../controllers/usercontroller.js';
 import { upload } from "../middlewares/upload.js";
 
-
+import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/signup", upload.single("profileImage"), signup);
 router.post('/login', loginUser);
-router.get('/singleuser/:userId', singleuser);
+router.get('/singleuser/:userId',protect, singleuser);
 
 export default router;
